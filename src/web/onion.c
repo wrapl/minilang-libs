ba03df0e2b77a9197fae4a877514ccc7f7971bfb
Raja Mukherji <rajamukherji@gmail.com>
2021-11-17 08:31:50 +0000

    Updates.

--- src/web/onion.c
+++ src/web/onion.c
@@ -1,6 +1,7 @@
-#define HAVE_PTHREADS
 #include <minilang/ml_library.h>
 #include <minilang/ml_macros.h>
+#include <minilang/ml_object.h>
+#define HAVE_PTHREADS
 #include <onion/onion.h>
 #include <onion/block.h>
 #include <onion/low.h>
@@ -12,20 +13,24 @@
 #include <string.h>
 #include <stdio.h>
 
-typedef struct ml_onion_t {
+typedef struct {
 	const ml_type_t *Type;
 	onion *Handle;
 } ml_onion_t;
 
-static ml_type_t *OnionT;
+extern ml_type_t OnionT[];
 
-static ml_value_t *ml_onion_new_fn(void *Data, int Count, ml_value_t **Args) {
+ML_FUNCTION(Onion) {
 	ml_onion_t *Onion = new(ml_onion_t);
 	Onion->Type = OnionT;
 	Onion->Handle = onion_new(Count >= 1 ? ml_integer_value(Args[0]) : 0);
 	onion_set_client_data(Onion->Handle, Onion, NULL);
 	return (ml_value_t *)Onion;
 }
+
+ML_TYPE(OnionT, (), "onion",
+	.Constructor = (ml_value_t *)Onion
+);
 
 ML_METHOD("listen", OnionT) {
 	ml_onion_t *Onion = (ml_onion_t *)Args[0];
@@ -38,12 +43,12 @@ ML_METHOD("listen_stop", OnionT) {
 	return MLNil;
 }
 
-typedef struct ml_onion_handler_t {
+typedef struct {
 	const ml_type_t *Type;
 	onion_handler *Handle;
 } ml_onion_handler_t;
 
-static ml_type_t *OnionHandlerT;
+ML_TYPE(OnionHandlerT, (), "onion-handler");
 
 ML_METHOD("get_root_handler", OnionT) {
 	ml_onion_t *Onion = (ml_onion_t *)Args[0];
@@ -79,12 +84,12 @@ ML_METHOD("set_port", OnionT, MLStringT) {
 	return Args[0];
 }
 
-typedef struct ml_onion_request_t {
+typedef struct {
 	const ml_type_t *Type;
 	onion_request *Handle;
 } ml_onion_request_t;
 
-static ml_type_t *OnionRequestT;
+ML_TYPE(OnionRequestT, (), "onion-request");
 
 ML_METHOD("get_path", OnionRequestT) {
 	ml_onion_request_t *Request = (ml_onion_request_t *)Args[0];
@@ -128,12 +133,12 @@ ML_METHOD("get_data", OnionRequestT) {
 	return ml_string(onion_block_data(Block), onion_block_size(Block));
 }
 
-typedef struct ml_onion_response_t {
+typedef struct {
 	const ml_type_t *Type;
 	onion_response *Handle;
 } ml_onion_response_t;
 
-static ml_type_t *OnionResponseT;
+ML_TYPE(OnionResponseT, (), "onion-response");
 
 ML_METHOD("set_code", OnionResponseT, MLIntegerT) {
 	ml_onion_response_t *Response = (ml_onion_response_t *)Args[0];
@@ -168,36 +173,27 @@ ML_METHOD("flush", OnionResponseT) {
 	return Args[0];
 }
 
-typedef struct ml_onion_state_t {
+typedef struct {
 	ml_state_t Base;
 	ml_value_t *Args[2];
 	onion_connection_status Status;
 } ml_onion_state_t;
 
-static ml_type_t *OnionStateT;
+ML_TYPE(OnionStateT, (), "onion-state");
 
-typedef struct ml_onion_connection_status_t {
-	const ml_type_t *Type;
-	const char *Name;
-	onion_connection_status Value;
-} ml_onion_connection_status_t;
-
-static ml_type_t *OnionConnectionStatusT;
-
-ML_METHOD("append", MLStringBufferT, OnionConnectionStatusT) {
-	ml_stringbuffer_t *Buffer = (ml_stringbuffer_t *)Args[0];
-	ml_onion_connection_status_t *Status = (ml_onion_connection_status_t *)Args[1];
-	ml_stringbuffer_write(Buffer, Status->Name, strlen(Status->Name));
-	return MLSome;
-}
-
-static ml_value_t *ml_onion_connection_status(const char *Name, onion_connection_status Value) {
-	ml_onion_connection_status_t *Status = new(ml_onion_connection_status_t);
-	Status->Type = OnionConnectionStatusT;
-	Status->Name = Name;
-	Status->Value = Value;
-	return (ml_value_t *)Status;
-}
+static ML_ENUM2(OnionConnectionStatusT,
+	"NotProcessed", OCS_NOT_PROCESSED,
+	"NeedMoreData", OCS_NEED_MORE_DATA,
+	"Processed", OCS_PROCESSED,
+	"CloseConnection", OCS_CLOSE_CONNECTION,
+	"KeepAlive", OCS_KEEP_ALIVE,
+	"Websocket", OCS_WEBSOCKET,
+	"RequestReady", OCS_REQUEST_READY,
+	"InternalError", OCS_INTERNAL_ERROR,
+	"NotImplemented", OCS_NOT_IMPLEMENTED,
+	"Forbidden", OCS_FORBIDDEN,
+	"Yield", OCS_YIELD
+);
 
 static ml_value_t *ml_onion_state_run(ml_onion_state_t *State, ml_value_t *Result) {
 	if (Result->Type == MLErrorT) {
@@ -212,7 +208,7 @@ static ml_value_t *ml_onion_state_run(ml_onion_state_t *State, ml_value_t *Resul
 	}
 	if (State->Status == OCS_NOT_PROCESSED) {
 		if (Result->Type == OnionConnectionStatusT) {
-			State->Status = ((ml_onion_connection_status_t *)Result)->Value;
+			State->Status = ml_enum_value(Result);
 		} else {
 			State->Status = OCS_PROCESSED;
 		}
@@ -247,27 +243,21 @@ static onion_connection_status ml_onion_handler(ml_value_t *Callback, onion_requ
 	}
 }
 
-static ml_value_t *ml_onion_handler_new_fn(void *Data, int Count, ml_value_t **Args) {
-	ML_CHECK_ARG_COUNT(1);
+ML_METHOD(OnionHandlerT, MLFunctionT) {
 	ml_onion_handler_t *Handler = new(ml_onion_handler_t);
 	Handler->Type = OnionHandlerT;
 	Handler->Handle = onion_handler_new((void *)ml_onion_handler, Args[0], NULL);
 	return (ml_value_t*)Handler;
 }
 
-static ml_value_t *ml_onion_export_local_fn(void *Data, int Count, ml_value_t **Args) {
-	ML_CHECK_ARG_COUNT(1);
-	ML_CHECK_ARG_TYPE(0, MLStringT);
+ML_METHOD(OnionHandlerT, MLStringT) {
 	ml_onion_handler_t *Handler = new(ml_onion_handler_t);
 	Handler->Type = OnionHandlerT;
 	Handler->Handle = onion_handler_export_local_new(ml_string_value(Args[0]));
 	return (ml_value_t *)Handler;
 }
 
-static ml_value_t *ml_onion_handler_static_fn(void *Data, int Count, ml_value_t **Args) {
-	ML_CHECK_ARG_COUNT(2);
-	ML_CHECK_ARG_TYPE(0, MLIntegerT);
-	ML_CHECK_ARG_TYPE(1, MLStringT);
+ML_METHOD(OnionHandlerT, MLIntegerT, MLStringT) {
 	ml_onion_handler_t *Handler = new(ml_onion_handler_t);
 	Handler->Type = OnionHandlerT;
 	Handler->Handle = onion_handler_static(ml_string_value(Args[1]), ml_integer_value(Args[0]));
@@ -286,7 +276,7 @@ typedef struct ml_onion_url_t {
 	onion_url *Handle;
 } ml_onion_url_t;
 
-static ml_type_t *OnionUrlT;
+ML_TYPE(OnionUrlT, (), "onion-url");
 
 ML_METHOD("root_url", OnionT) {
 	ml_onion_t *Onion = (ml_onion_t *)Args[0];
@@ -337,7 +327,7 @@ typedef struct ml_onion_websocket_t {
 	ml_stringbuffer_t Buffer[1];
 } ml_onion_websocket_t;
 
-static ml_type_t *OnionWebsocketT;
+extern ml_type_t OnionWebsocketT[];
 
 static onion_connection_status ml_onion_websocket_callback(ml_onion_websocket_t *Websocket, onion_websocket *Handle, ssize_t DataReadyLength) {
 	if (DataReadyLength >= 0) {
@@ -372,21 +362,21 @@ static onion_connection_status ml_onion_websocket_callback(ml_onion_websocket_t 
 			return OCS_INTERNAL_ERROR;
 		}
 		if (Result->Type == OnionConnectionStatusT) {
-			return ((ml_onion_connection_status_t *)Result)->Value;
+			return ml_enum_value(Result);
 		} else {
 			return OCS_NEED_MORE_DATA;
 		}
 	} else {
 		ml_value_t *Result = ml_simple_inline(Websocket->Callback, 1, MLNil);
 		if (Result->Type == OnionConnectionStatusT) {
-			return ((ml_onion_connection_status_t *)Result)->Value;
+			return ml_enum_value(Result);
 		} else {
 			return OCS_CLOSE_CONNECTION;
 		}
 	}
 }
 
-static ml_value_t *ml_websocket_new_fn(void *Data, int Count, ml_value_t **Args) {
+ML_FUNCTION(OnionWebsocket) {
 	ML_CHECK_ARG_COUNT(2);
 	ML_CHECK_ARG_TYPE(0, OnionRequestT);
 	ML_CHECK_ARG_TYPE(1, OnionResponseT);
@@ -402,32 +392,27 @@ static ml_value_t *ml_websocket_new_fn(void *Data, int Count, ml_value_t **Args)
 	return (ml_value_t *)Websocket;
 }
 
+ML_TYPE(OnionWebsocketT, (), "onion-websocket",
+	.Constructor = (ml_value_t *)OnionWebsocket
+);
+
 ML_METHOD("set_callback", OnionWebsocketT, MLFunctionT) {
 	ml_onion_websocket_t *Websocket = (ml_onion_websocket_t *)Args[0];
 	Websocket->Callback = Args[1];
 	return Args[0];
 }
 
-typedef struct ml_onion_websocket_opcode_t {
-	const ml_type_t *Type;
-	const char *Name;
-	onion_websocket_opcode Value;
-} ml_onion_websocket_opcode_t;
-
-static ml_type_t *OnionWebsocketOpcodeT;
-
-static ml_value_t *ml_onion_websocket_opcode(const char *Name, onion_websocket_opcode Value) {
-	ml_onion_websocket_opcode_t *Opcode = new(ml_onion_websocket_opcode_t);
-	Opcode->Type = OnionWebsocketOpcodeT;
-	Opcode->Name = Name;
-	Opcode->Value = Value;
-	return (ml_value_t *)Opcode;
-}
+static ML_ENUM2(OnionWebsocketOpcodeT,
+	"Text", OWS_TEXT,
+	"Binary", OWS_BINARY,
+	"ConnectionClose", OWS_CONNECTION_CLOSE,
+	"Ping", OWS_PING,
+	"Pong", OWS_PONG
+);
 
 ML_METHOD("set_opcode", OnionWebsocketT, OnionWebsocketOpcodeT) {
 	ml_onion_websocket_t *Websocket = (ml_onion_websocket_t *)Args[0];
-	ml_onion_websocket_opcode_t *Opcode = (ml_onion_websocket_opcode_t *)Args[1];
-	onion_websocket_set_opcode(Websocket->Handle, Opcode->Value);
+	onion_websocket_set_opcode(Websocket->Handle, ml_enum_value(Args[1]));
 	return Args[0];
 }
 
@@ -457,39 +442,10 @@ void ml_library_entry0(ml_value_t **Slot) {
 		GC_pthread_cancel, GC_pthread_detach,
 		GC_pthread_exit, GC_pthread_sigmask
 	);
-	OnionT = ml_type(MLAnyT, "onion");
-	OnionHandlerT = ml_type(MLAnyT, "onion-handler");
-	OnionUrlT = ml_type(MLAnyT, "onion-url");
-	OnionRequestT = ml_type(MLAnyT, "onion-request");
-	OnionResponseT = ml_type(MLAnyT, "onion-response");
-	OnionStateT = ml_type(MLStateT, "onion-state");
-	OnionWebsocketT = ml_type(MLAnyT, "onion-websocket");
 #include "onion_init.c"
-	Slot[0] = ml_module("onion",
-		"new", ml_cfunction(NULL, ml_onion_new_fn),
-		"handler", ml_cfunction(NULL, ml_onion_handler_new_fn),
-		"local", ml_cfunction(NULL, ml_onion_export_local_fn),
-		"static", ml_cfunction(NULL, ml_onion_handler_static_fn),
-		"websocket", ml_cfunction(NULL, ml_websocket_new_fn),
-		"connection_status", ml_module("connection_status",
-			"NotProcessed", ml_onion_connection_status("NotProcessed", OCS_NOT_PROCESSED),
-			"NeedMoreData", ml_onion_connection_status("NeedMoreData", OCS_NEED_MORE_DATA),
-			"Processed", ml_onion_connection_status("Processed", OCS_PROCESSED),
-			"CloseConnection", ml_onion_connection_status("CloseConnection", OCS_CLOSE_CONNECTION),
-			"KeepAlive", ml_onion_connection_status("KeepAlive", OCS_KEEP_ALIVE),
-			"Websocket", ml_onion_connection_status("Websocket", OCS_WEBSOCKET),
-			"RequestReady", ml_onion_connection_status("RequestReady", OCS_REQUEST_READY),
-			"InternalError", ml_onion_connection_status("InternalError", OCS_INTERNAL_ERROR),
-			"NotImplemented", ml_onion_connection_status("NotImplemented", OCS_NOT_IMPLEMENTED),
-			"Forbidden", ml_onion_connection_status("Forbidden", OCS_FORBIDDEN),
-			"Yield", ml_onion_connection_status("Yield", OCS_YIELD),
-		NULL),
-		"websocket_opcode", ml_module("websocket_opcode",
-			"Text", ml_onion_websocket_opcode("Text", OWS_TEXT),
-			"Binary", ml_onion_websocket_opcode("Text", OWS_BINARY),
-			"ConnectionClose", ml_onion_websocket_opcode("Text", OWS_CONNECTION_CLOSE),
-			"Ping", ml_onion_websocket_opcode("Text", OWS_PING),
-			"Pong", ml_onion_websocket_opcode("Text", OWS_PONG),
-		NULL),
-	NULL);
+	stringmap_insert(OnionT->Exports, "handler", OnionHandlerT);
+	stringmap_insert(OnionT->Exports, "websocket", OnionWebsocketT);
+	stringmap_insert(OnionT->Exports, "connection_status", OnionConnectionStatusT);
+	stringmap_insert(OnionT->Exports, "websocket_opcode", OnionWebsocketOpcodeT);
+	Slot[0] = (ml_value_t *)OnionT;
 }

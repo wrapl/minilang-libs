b5ac2b96fba8bd51393d1791be76e4cf0f10222b
Raja Mukherji <rajamukherji@gmail.com>
2023-03-13 02:59:43 +0000

    Updates.

--- src/event/uev.c
+++ src/event/uev.c
@@ -7,6 +7,7 @@
 typedef struct {
 	ml_type_t *Type;
 	uev_ctx_t Handle[1];
+	int Running;
 } ml_uev_t;
 
 ML_TYPE(UevT, (), "uev");
@@ -20,12 +21,21 @@ ML_METHOD(UevT) {
 
 ML_METHOD("run", UevT) {
 	ml_uev_t *Context = (ml_uev_t *)Args[0];
-	uev_run(Context->Handle, 0);
+	Context->Running = 1;
+	do {
+		uev_run(Context->Handle, UEV_ONCE);
+		ml_queued_state_t Queued = ml_default_queue_next();
+		while (Queued.State) {
+			Queued.State->run(Queued.State, Queued.Value);
+			Queued = ml_default_queue_next();
+		}
+	} while (Context->Running);
 	return MLNil;
 }
 
 ML_METHOD("exit", UevT) {
 	ml_uev_t *Context = (ml_uev_t *)Args[0];
+	Context->Running = 0;
 	uev_exit(Context->Handle);
 	return MLNil;
 }
@@ -42,8 +52,7 @@ ML_FLAGS2(UevFlagsT, "uev::flags",
 );
 
 typedef struct {
-	ml_type_t *Type;
-	ml_context_t *Context;
+	ml_state_t Base;
 	ml_value_t *Function;
 	int (*start)(uev_t *);
 	int (*stop)(uev_t *);
@@ -62,20 +71,15 @@ ML_METHOD("stop", UevEventT) {
 	return ml_integer(Event->stop(Event->Handle));
 }
 
-static void uev_callback(uev_t *Handle, void *Data, int Events) {
+static void ml_uev_event_run(ml_uev_event_t *State, ml_value_t *Value) {
+	// TODO: check for errors and send somewhere
+}
+
+static void ml_uev_callback(uev_t *Handle, void *Data, int Events) {
 	ml_uev_event_t *Event = (ml_uev_event_t *)Data;
-	ml_result_state_t *State = ml_result_state(Event->Context);
 	ml_value_t **Args = ml_alloc_args(1);
 	Args[0] = ml_flags_value(UevFlagsT, Events);
-	ml_call(State, Event->Function, 1, Args);
-	while (!State->Value) {
-		ml_queued_state_t Queued = ml_default_queue_next();
-		if (Queued.State) {
-			Queued.State->run(Queued.State, Queued.Value);
-			continue;
-		}
-		uev_run(Handle->ctx, UEV_ONCE);
-	}
+	return ml_call((ml_state_t *)Event, Event->Function, 1, Args);
 }
 
 ML_METHODX("io", UevT, MLStreamFdT, UevFlagsT, MLFunctionT) {
@@ -83,12 +87,13 @@ ML_METHODX("io", UevT, MLStreamFdT, UevFlagsT, MLFunctionT) {
 	int Fd = ml_fd_stream_fd(Args[1]);
 	int Flags = ml_flags_value_value(Args[2]);
 	ml_uev_event_t *Event = new(ml_uev_event_t);
-	Event->Type = UevEventT;
-	Event->Context = Caller->Context;
+	Event->Base.Type = UevEventT;
+	Event->Base.Context = Caller->Context;
+	Event->Base.run = (ml_state_fn)ml_uev_event_run;
 	Event->Function = Args[3];
 	Event->start = uev_io_start;
 	Event->stop = uev_io_stop;
-	uev_io_init(Context->Handle, Event->Handle, uev_callback, Event, Fd, Flags);
+	uev_io_init(Context->Handle, Event->Handle, ml_uev_callback, Event, Fd, Flags);
 	ML_RETURN(Event);
 }
 
@@ -96,12 +101,13 @@ ML_METHODX("timer", UevT, MLIntegerT, MLFunctionT) {
 	ml_uev_t *Context = (ml_uev_t *)Args[0];
 	int Timeout = ml_integer_value(Args[1]);
 	ml_uev_event_t *Event = new(ml_uev_event_t);
-	Event->Type = UevEventT;
-	Event->Context = Caller->Context;
+	Event->Base.Type = UevEventT;
+	Event->Base.Context = Caller->Context;
+	Event->Base.run = (ml_state_fn)ml_uev_event_run;
 	Event->Function = Args[2];
 	Event->start = uev_timer_start;
 	Event->stop = uev_timer_stop;
-	uev_timer_init(Context->Handle, Event->Handle, uev_callback, Event, Timeout, 0);
+	uev_timer_init(Context->Handle, Event->Handle, ml_uev_callback, Event, Timeout, 0);
 	ML_RETURN(Event);
 }
 
@@ -110,12 +116,26 @@ ML_METHODX("timer", UevT, MLIntegerT, MLIntegerT, MLFunctionT) {
 	int Timeout = ml_integer_value(Args[1]);
 	int Period = ml_integer_value(Args[2]);
 	ml_uev_event_t *Event = new(ml_uev_event_t);
-	Event->Type = UevEventT;
-	Event->Context = Caller->Context;
+	Event->Base.Type = UevEventT;
+	Event->Base.Context = Caller->Context;
+	Event->Base.run = (ml_state_fn)ml_uev_event_run;
 	Event->Function = Args[3];
 	Event->start = uev_timer_start;
 	Event->stop = uev_timer_stop;
-	uev_timer_init(Context->Handle, Event->Handle, uev_callback, Event, Timeout, Period);
+	uev_timer_init(Context->Handle, Event->Handle, ml_uev_callback, Event, Timeout, Period);
+	ML_RETURN(Event);
+}
+
+ML_METHODX("event", UevT, MLFunctionT) {
+	ml_uev_t *Context = (ml_uev_t *)Args[0];
+	ml_uev_event_t *Event = new(ml_uev_event_t);
+	Event->Base.Type = UevEventT;
+	Event->Base.Context = Caller->Context;
+	Event->Base.run = (ml_state_fn)ml_uev_event_run;
+	Event->Function = Args[3];
+	Event->start = uev_event_post;
+	Event->stop = uev_event_stop;
+	uev_event_init(Context->Handle, Event->Handle, ml_uev_callback, Event);
 	ML_RETURN(Event);
 }
 

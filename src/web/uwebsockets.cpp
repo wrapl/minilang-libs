4edbb4d4d0ea2903f80aeb931ce3f52c38e63e14
rajamukherji@gmail.com <rajamukherji@gmail.com>
2026-01-03 07:53:22 +0000

    Updates.

--- src/web/uwebsockets.cpp
+++ src/web/uwebsockets.cpp
@@ -4,6 +4,8 @@
 #include <gc/gc_cpp.h>
 #include <uWebSockets/App.h>
 
+#define anew(T, N) ((T *)GC_MALLOC((N) * sizeof(T)))
+
 #undef ML_CATEGORY
 #define ML_CATEGORY "web/uwebsockets"
 
@@ -11,7 +13,7 @@ ML_TYPE(AppT, (), "uwebsockets::app");
 
 ML_TYPE(AppSSLT, (AppT), "uwebsockets::app::ssl");
 
-template<bool SSL> struct app_t {
+template<bool SSL> struct app_t : gc_cleanup {
 	ml_type_t *Type;
 	uWS::TemplatedApp<SSL> Handle;
 
@@ -25,18 +27,64 @@ template<bool SSL> struct app_t {
 };
 
 ML_METHOD(AppT) {
-	auto App = new (GC) app_t<false>;
+	auto App = new app_t<false>();
 	return (ml_value_t *)App;
 }
 
-ML_METHOD("get", AppT, MLStringT, MLFunctionT) {
-	auto App = (app_t<false> *)Args[0];
+ML_TYPE(ResponseT, (), "uwebsockets::response");
+
+ML_TYPE(ResponseSSLT, (ResponseT), "uwebsockets::response");
+
+template<bool SSL> struct response_t : gc_cleanup {
+	ml_type_t *Type;
+	uWS::HttpResponse<SSL> *Handle;
+
+	response_t(uWS::HttpResponse<SSL> *Handle) : Handle(Handle) {
+		Type = ResponseT;
+	}
+
+	~response_t() {
+		if (Handle) Handle->tryEnd("", 0, true);
+	}
+};
+
+static ml_value_t *ml_string_copy(const std::string_view &View) {
+	int Size = View.size();
+	char Chars[Size + 1];
+	View.copy(Chars, Size);
+	Chars[Size] = 0;
+	return ml_string_copy(Chars, Size);
+}
+
+template<bool SSL> static ml_value_t *app_get(ml_value_t **Args) {
+	auto App = (app_t<SSL> *)Args[0];
 	std::string Path(ml_string_value(Args[1]));
-	App->Handle.get(Path, [](auto *Res, auto *Req) {
+	App->Handle.get(Path, [](uWS::HttpResponse<SSL> *Res, uWS::HttpRequest *Req) {
+		ml_value_t **Args = ml_alloc_args(6);
+		Args[0] = ml_string_copy(Req->getUrl());
+		Args[1] = ml_string_copy(Req->getQuery());
+		ml_value_t *Headers = Args[2] = ml_list();
+		for (auto Header : Req->headers) {
+			ml_value_t *Key = ml_string_copy(Header.key);
+			ml_value_t *Value = ml_string_copy(Header.value);
+			ml_list_put(Headers, ml_tuplev(2, Key, Value));
+		}
+
 	});
+	return Args[0];
+}
+
+ML_METHOD("get", AppT, MLStringT, MLFunctionT) {
+	return app_get<false>(Args);
+}
+
+ML_METHOD("get", AppSSLT, MLStringT, MLFunctionT) {
+	return app_get<true>(Args);
 }
 
 ML_LIBRARY_ENTRY0(web_uwebsockets) {
 #include "uwebsockets_init.cpp"
-	Slot[0] = (ml_value_t *)AppT;
+	Slot[0] = ml_module("uwebsockets",
+		"app", AppT,
+	NULL);
 }
